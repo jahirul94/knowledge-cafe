5ee4ee55dd9c5d4e950b7b79e12e5c90b6053d0a
jahirul94 <jahirulislam41185@gmail.com>
2023-04-01 16:54:24 +0600

    complate this project

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div>
             <nav className='nav-bar'>
-                <h1> Knowledge Cafe </h1>
+                <h1>Explore Knowledge Cafe</h1>
                  <div className='header'>
                     <div className='nav-link'>
                         <a href="./home">Home</a>
@@ -12,12 +12,11 @@ const Header = () => {
                         <a href="./contact">Contact Us</a>
                     </div>
                      <div>
-                        <img className='header-img' src="../../../public/user-image/user-7.jpg" alt="" />
+                        <img className='header-img' src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=740&t=st=1680346086~exp=1680346686~hmac=d7391a302bb0f7bbaf89431c22e90dea64e2e71f35a6c7d0220d8268a5f4b310" alt="" />
                      </div>
                  </div>
             </nav>    
         </div>
     );
 };
-
 export default Header;
